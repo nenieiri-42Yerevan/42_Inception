0a8b5a2b1baa460d55fe3a20a5ae5b6ec790f812
Volodya <nenie_iri@mail.ru>
2022-12-30 00:56:34 +0400

    mariadb

--- srcs/requirements/mariadb/tools/entrypoint-script.sh
+++ srcs/requirements/mariadb/tools/entrypoint-script.sh
@@ -2,22 +2,20 @@
 
 cd /tmp;
 
-service mysql start;
+mysql_install_db;
 
-mysql_install_db --datadir=/var/lib/mysql;
-
-echo "CREATE DATABASE $MYSQL_DATABASE;" > ./install.sql;
-
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > ./install.sql;
 echo -n "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* " >> ./install.sql;
 echo "TO $MYSQL_USER@localhost IDENTIFIED BY '$MYSQL_PASSWORD';" >> ./install.sql;
+echo "FLUSH PRIVILEGES;" >> ./install.sql;
 
 echo -n "ALTER USER 'root'@'localhost' " >> ./install.sql;
 echo "IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> ./install.sql;
-
 echo "FLUSH PRIVILEGES;" >> ./install.sql;
 
-mariadb < ./install.sql;
+service mysql start;
+mariadb -u root --password=$MYSQL_ROOT_PASSWORD < install.sql;
 
-rm -f ./install.sql;
+exec mysqld --init-file=/tmp/install.sql
 
-exec "$@";
+#exec "$@";

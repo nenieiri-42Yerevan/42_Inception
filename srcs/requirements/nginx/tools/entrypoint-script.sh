23de33e4fa210f42d502f8886dcdc72008c68088
Volodya Ismailyan <nenie_iri@mail.ru>
2022-12-31 20:14:27 +0400

    connect nginx to wordpress

--- srcs/requirements/nginx/tools/entrypoint-script.sh
+++ srcs/requirements/nginx/tools/entrypoint-script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+sed -i "s/DOMAIN_NAME/$DOMAIN_NAME/g"	  /etc/nginx/sites-available/nginx.conf;
+sed -i "s/WORDPRESS_NET/$WORDPRESS_NET/g" /etc/nginx/sites-available/nginx.conf;
+
 exec "$@";
--- srcs/requirements/wordpress/tools/entrypoint-script.sh
+++ srcs/requirements/wordpress/tools/entrypoint-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i "s/_data_sql/$MYSQL_DATABASE/g" wp-config.php;
-sed -i "s/_username/$MYSQL_USER/g"	   wp-config.php;
-sed -i "s/_password/$MYSQL_PASSWORD/g" wp-config.php;
-sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config.php;
+sed -i "s/_data_sql/$MYSQL_DATABASE/g" ./wordpress/wp-config.php;
+sed -i "s/_username/$MYSQL_USER/g"	   ./wordpress/wp-config.php;
+sed -i "s/_password/$MYSQL_PASSWORD/g" ./wordpress/wp-config.php;
+sed -i "s/localhost/$MYSQL_HOSTNAME/g" ./wordpress/wp-config.php;
 
 exec "$@";

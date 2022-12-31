e52bb9929f7bd1128652a4ce39626054430d966a
Volodya Ismailyan <nenie_iri@mail.ru>
2022-12-31 21:18:05 +0400

    nginx: finish

--- srcs/requirements/nginx/tools/entrypoint-script.sh
+++ srcs/requirements/nginx/tools/entrypoint-script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sed -i "s/DOMAIN_NAME/$DOMAIN_NAME/g"	  /etc/nginx/sites-available/nginx.conf;
-sed -i "s/WORDPRESS_NET/$WORDPRESS_NET/g" /etc/nginx/sites-available/nginx.conf;
+nginx=/etc/nginx/sites-available/nginx.conf;
+
+sed -i "s=CERTIFICATE_ENV=$CERTIFICATE=g"			$nginx;
+sed -i "s=CERTIFICATE_KEY_ENV=$CERTIFICATE_KEY=g"	$nginx;
+sed -i "s=DOMAIN_NAME=$DOMAIN_NAME=g"				$nginx;
+sed -i "s=WORDPRESS_NET=$WORDPRESS_NET=g"			$nginx;
 
 exec "$@";

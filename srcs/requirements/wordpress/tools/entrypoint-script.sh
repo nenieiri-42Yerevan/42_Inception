2739e86154c68790567f1798e350e22732afd599
Volodya Ismailyan <nenie_iri@mail.ru>
2023-01-06 12:00:06 +0400

    add wp-cli

--- srcs/requirements/wordpress/tools/entrypoint-script.sh
+++ srcs/requirements/wordpress/tools/entrypoint-script.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-sed -i "s/_data_sql/$MYSQL_DATABASE/g" ./wordpress/wp-config.php;
-sed -i "s/_username/$MYSQL_USER/g"	   ./wordpress/wp-config.php;
-sed -i "s/_password/$MYSQL_PASSWORD/g" ./wordpress/wp-config.php;
-sed -i "s/localhost/$MYSQL_HOSTNAME/g" ./wordpress/wp-config.php;
+if ! [ -f "/var/www/wordpress/wp-config.php" ]; then
+	sed -i "s/_data_sql/$MYSQL_DATABASE/g" ./wordpress/wp-config.php;
+	sed -i "s/_username/$MYSQL_USER/g"	   ./wordpress/wp-config.php;
+	sed -i "s/_password/$MYSQL_PASSWORD/g" ./wordpress/wp-config.php;
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" ./wordpress/wp-config.php;
+
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+	chmod +x wp-cli.phar
+
+	mv wp-cli.phar /usr/local/bin/wp
+fi
 
 exec "$@";
